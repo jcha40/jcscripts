a2b0174279dc9fa6cbf8afcbcefecd13dcaa2e1f
jcha40 <sungsoocha97@gmail.com>
2023-02-20 13:35:42 -0500

    Initialize encoding as arrays to improve speed

--- sequence_manipulation/onehot.py
+++ sequence_manipulation/onehot.py
@@ -1,4 +1,5 @@
 import numpy as np
 
-def onehot(seq, alphadict={'A': (1, 0, 0, 0), 'C': (0, 1, 0, 0), 'G': (0, 0, 1, 0), 'T': (0, 0, 0, 1), 'N': (0, 0, 0, 0)}):
+def onehot(seq, alphadict={'A': np.array([1, 0, 0, 0]), 'C': np.array([0, 1, 0, 0]), 'G': np.array([0, 0, 1, 0]),
+                           'T': np.array([0, 0, 0, 1]), 'N': np.array([0, 0, 0, 0])}):
     return np.array([alphadict[letter] for letter in seq])
