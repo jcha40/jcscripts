5c2ec7453efee6e3ebb570722595c6338be433b5
jcha40 <sungsoocha97@gmail.com>
2024-02-06 13:20:06 -0500

    Corrected name since it only works for x=.5

--- math/logbetai.jl
+++ math/logbetai.jl
@@ -1,15 +1,15 @@
 using SpecialFunctions
 
-function logbetai(x::Float64, a::Float64, b::Float64; n::Int64 = 20)::Float64
+function logbetah(a::Float64, b::Float64; n::Int64 = 20, loghalf::Float64 = log(.5))::Float64
     """
-    Compute the logarithm of the incomplete regularized beta function using the continued fraction representation.
+    Compute the logarithm of the regularized half-beta function using the continued fraction representation.
     """
     if a < b
-        return log1p(-exp(logbetai(x, b, a, n=n)))
+        return log1p(-exp(logbetah(b, a, n=n)))
     end
     frac = 1.
     for k in n:-1:1
-        frac = 1 - (((a + k - 1) * (a + b + k - 1) * x) / ((a + k * 2 - 2) * (a + k * 2 - 1))) / (1 + ((k * (b - k) * x) / ((a + k * 2 - 1) * (a + k * 2))) / frac)
+        frac = 1 - (((a + k - 1) * (a + b + k - 1) * .5) / ((a + k * 2 - 2) * (a + k * 2 - 1))) / (1 + ((k * (b - k) * .5) / ((a + k * 2 - 1) * (a + k * 2))) / frac)
     end
-    return a * log(x) + b * log(1 - x) - log(a) - logbeta(a, b) - log(frac)
+    return a * loghalf + b * loghalf - log(a) - logbeta(a, b) - log(frac)
 end
