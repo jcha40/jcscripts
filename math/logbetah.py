be4a0700a0f5ccdf00497f3afe9c1f1ff2770bf3
jcha40 <sungsoocha97@gmail.com>
2026-04-07 14:12:45 -0400

    Empty is faster than zeros

--- math/logbetah.py
+++ math/logbetah.py
@@ -1,4 +1,4 @@
-from numpy import exp, log, log1p, ndarray, zeros_like, float64
+from numpy import exp, log, log1p, ndarray, empty_like, float64
 from scipy.special import betaln
 
 _loghalf = log(.5)
@@ -23,7 +23,7 @@ def logbetah(a, b, n=20):
             frac = 1 - (((a + k - 1) * (a + b + k - 1) * .5) / ((a + k * 2 - 2) * (a + k * 2 - 1))) / (1 + ((k * (b - k) * .5) / ((a + k * 2 - 1) * (a + k * 2))) / frac)
         return a * _loghalf + b * _loghalf - log(a) - betaln(a, b) - log(frac)
 
-    ans = zeros_like(a + b, dtype=float64)
+    ans = empty_like(a + b, dtype=float64)
     mask = a < b
     if isinstance(a, (int, float)):
         a_lt = a_ge = a
