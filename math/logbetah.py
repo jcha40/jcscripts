4777930ef67ad0c8500e11b65f042bcf498ed4e7
jcha40 <sungsoocha97@gmail.com>
2026-04-07 14:17:58 -0400

    Reordered code

--- math/logbetah.py
+++ math/logbetah.py
@@ -23,7 +23,6 @@ def logbetah(a, b, n=20):
             frac = 1 - (((a + k - 1) * (a + b + k - 1) * .5) / ((a + k * 2 - 2) * (a + k * 2 - 1))) / (1 + ((k * (b - k) * .5) / ((a + k * 2 - 1) * (a + k * 2))) / frac)
         return a * _loghalf + b * _loghalf - log(a) - betaln(a, b) - log(frac)
 
-    ans = empty_like(a + b, dtype=float64)
     mask = a < b
     if isinstance(a, (int, float)):
         a_lt = a_ge = a
@@ -36,9 +35,9 @@ def logbetah(a, b, n=20):
         b_lt = b[mask]
         b_ge = b[~mask]
     
+    ans = empty_like(a + b, dtype=float64)
     if mask.any():
         ans[mask] = log1p(-exp(logbetah(b_lt, a_lt, n=n)))
-
     frac = 1.
     for k in range(n, 0, -1):
         frac = 1 - (((a_ge + k - 1) * (a_ge + b_ge + k - 1) * .5) / ((a_ge + k * 2 - 2) * (a_ge + k * 2 - 1))) / (1 + ((k * (b_ge - k) * .5) / ((a_ge + k * 2 - 1) * (a_ge + k * 2))) / frac)
