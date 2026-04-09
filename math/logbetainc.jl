85ea51a1d4ae474f092e2f98c1d6452065fa69f1
jcha40 <sungsoocha97@gmail.com>
2026-04-09 13:48:48 -0400

    Changed variable switch condition to match https://en.wikipedia.org/wiki/Beta_function#Continued_fraction_expansion

--- math/logbetainc.jl
+++ math/logbetainc.jl
@@ -7,7 +7,7 @@ function logbetainc(a::Real, b::Real, x::Real; n::Int64 = 20, min_x_swap::Float6
     if x < 0. || x > 1. || a <= 0. || b <= 0.
         return NaN
     end
-    if (a * (1. - x) < b * x) && x >= min_x_swap
+    if ((a + 1.) * (1. - x) < (b + 1.) * x) && x >= min_x_swap
         return log1p(-exp(logbetainc(b, a, 1. - x, n=n)))
     end
     frac = 1.
